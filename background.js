f3e28b99ff074d48d1e7e7ef6e2095d4caaa7e3c
Paul G <disegnovitruviano@users.noreply.github.com>
2023-07-28 01:29:41 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,25 @@
-// Handle browser action click
-chrome.browserAction.onClicked.addListener(() => {
-  chrome.tabs.create({ url: chrome.runtime.getURL('popup.html') });
+// Event listener for the browser action click
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: fetchAndDisplayDesignPrinciples,
+  });
 });
+
+// Function to fetch and display design principles
+function fetchAndDisplayDesignPrinciples() {
+  fetch('http://localhost:3000/api/design-principles')
+    .then(response => response.json())
+    .then(principles => {
+      const principlesContainer = document.createElement('div');
+
+      principles.forEach(principle => {
+        const principleElement = document.createElement('div');
+        principleElement.textContent = principle.title;
+        principlesContainer.appendChild(principleElement);
+      });
+
+      chrome.action.setPopup({ tabId: tab.id, popup: principlesContainer.outerHTML });
+    })
+    .catch(error => console.error(error));
+}
